3ed7ef89f5d2f150402d15d977016b1a720ba0fa
viniciusbrito1 <viniciusbrito13@hotmail.com>
2023-11-09 23:58:08 -0300

    Update GlobalKeyListener.js

--- src/components/GlobalKeyListener.js
+++ src/components/GlobalKeyListener.js
@@ -1,35 +1,41 @@
 import React, { useEffect } from 'react';
 
-function GlobalKeyListener({x,y,SetX,SetY}){
+const sendKeyPress = async (keyPressed,SetX,SetY) => {
+  const nome ="brito"
+  try {
+    const response = await fetch('http://localhost:3001/api/move', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({ nome , keyPressed }),
+    })
+    const json = await response.json()
+    SetX(json.x)
+    SetY(json.y)
+  } catch (err) {
+    console.log(err)
+  }
+
+  return false
+}
+
+function GlobalKeyListener({x,y,SetY,SetX}){
   const handleKeyPress = (event) => {
-    console.log('Key pressed globally:', event.key);
-    const delta = 10
-    const keyActions = {
-        'w' : () => SetY(y-delta),
-        's' : () => SetY(y+delta),
-        'a' : () => SetX(x-delta),
-        'd' : () => SetX(x+delta),
-    }
-
-    const action = keyActions[event.key];
-
-    if (action) {
-        action();
-    } else {
-        console.log("Invalid Key Pressed")
-    }
+    sendKeyPress(event.key,SetX,SetY)
   };
-
+  
   useEffect(() => {
     // Add the event listener when the component mounts
     document.addEventListener('keydown', handleKeyPress);
-
+    
     // Remove the event listener when the component unmounts
     return () => {
       document.removeEventListener('keydown', handleKeyPress);
     };
   }, [x, y, SetY]); // The empty dependency array ensures the effect runs only once on mount
-
+  
   return <></>
 };
 
